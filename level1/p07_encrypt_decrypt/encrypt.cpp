ba675051dd3041d494da3493fa75fccbd3719bac
Liyuhao2018081312004 <48055100+Liyuhao2018081312004@users.noreply.github.com>
2019-04-11 19:17:31 +0800

    Add files via upload

---
+++ level1/p07_encrypt_decrypt/encrypt.cpp
@@ -0,0 +1,14 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main()
+{
+	string s,o;
+	cin >> s;
+    int l = s.length();
+	for(int i = 0;i < l; i++ ){
+		o[i] = 256 - s[i];
+		cout << o[i];
+	}
+    return 0;  
+ } 
